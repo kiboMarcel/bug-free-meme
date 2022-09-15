8e81b474728a8e30c0e19dcba2b91bd42fd3b9c2
nouletame <nouletamemarcel@gmail.com>
2022-09-15 14:58:18 +0000

    icon_rotation

--- lib/rotate_logo.dart
+++ lib/rotate_logo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'dart:math' as math;
 
 class RotateLogoScreen extends StatefulWidget {
   const RotateLogoScreen({super.key});
@@ -10,34 +11,49 @@ class RotateLogoScreen extends StatefulWidget {
 
 class _RotateLogoScreenState extends State<RotateLogoScreen>
     with TickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> animation;
+  /* late AnimationController _animationController;
+  late Animation<double> animation; */
   late ScrollController _scrollController;
-  late ScrollDirection _scrollDirection;
+
+  final double initialImageRotationAngle = 0;
+  late ValueNotifier<double> imageRotationAngleNotifier;
+
+  void scrollListener() {
+    ScrollDirection scrollDirection =
+        _scrollController.position.userScrollDirection;
+    double scrollPosition = _scrollController.position.pixels.abs();
+    print(scrollPosition);
+    if (scrollDirection == ScrollDirection.forward) {
+      imageRotationAngleNotifier.value += scrollPosition * 0.01 * math.pi / 180;
+    } else {
+      //print(scrollPosition);
+      imageRotationAngleNotifier.value -= scrollPosition * 0.01 * math.pi / 180;
+    }
+  }
 
   @override
   void initState() {
-    _animationController = AnimationController(
+    /* _animationController = AnimationController(
       vsync: this,
-      duration: Duration(milliseconds: 2000),
+      duration: Duration(microseconds: 3000),
     );
 
-    animation = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+    animation = Tween(begin: 0.0, end: 100.0).animate(_animationController);
 
-    _animationController.forward();
+    _animationController.forward(); */
 
-    _scrollController = ScrollController()
-      ..addListener(() {
-        if (_scrollDirection == ScrollDirection.forward) {
-          _animationController.forward();
-          print('forward');
-        } else if (_scrollDirection == ScrollDirection.reverse) {
-          _animationController.reverse();
-          print('reverse');
-        }
-      });
+    _scrollController = ScrollController()..addListener(scrollListener);
+
+    imageRotationAngleNotifier =
+        ValueNotifier<double>(initialImageRotationAngle);
 
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(scrollListener);
+    super.dispose();
   }
 
   @override
@@ -55,35 +71,40 @@ class _RotateLogoScreenState extends State<RotateLogoScreen>
                 bottomRight: Radius.circular(15),
               ),
             ),
-            child: Transform.rotate(
-              angle: animation.value,
-              child: Icon(
+            child: ValueListenableBuilder(
+                valueListenable: imageRotationAngleNotifier,
+                builder: (context, double imageRotationAngle, child) {
+                  return Transform.rotate(
+                    angle: imageRotationAngleNotifier.value,
+                    child: Icon(
+                      Icons.circle_notifications,
+                      color: Colors.white,
+                      size: 80,
+                    ),
+                  );
+                }
+                /* child: Icon(
                 Icons.timelapse,
                 color: Colors.white,
                 size: 80,
-              ),
-            ),
+              ), */
+                ),
           ),
           SizedBox(
             height: 20,
           ),
           Expanded(
-              child: NotificationListener<UserScrollNotification>(
-            onNotification: (notification) {
-              setState(() {
-                _scrollDirection = notification.direction;
-              });
-              return true;
-            },
-            child: ListView(
-              controller: _scrollController,
-              children: [
-                for (var i = 0; i < 100; i++)
-                  ListTile(
-                    title: Text('item $i'),
-                  )
-              ],
-            ),
+              child: ListView(
+            controller: _scrollController,
+            children: [
+              for (var i = 0; i < 100; i++)
+                ListTile(
+                  title: Text('item $i'),
+                  onTap: () {
+                    print(math.pi);
+                  },
+                )
+            ],
           ))
         ],
       ),
